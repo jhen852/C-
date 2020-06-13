dcfac68677c4d2f790f7559401d53845b7e1e28e
jhen852 <jhenny.callapa.quispe@gmail.com>
2020-06-13 13:35:14 -0400

    version 1

---
+++ Clase_1/Clase_1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Usuario
+ * Date: 13/06/2020
+ * Time: 01:28 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Clase_1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
